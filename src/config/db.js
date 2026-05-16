3f721d89eb69dd472fecb916a981d548d2d06494
coder <stevenabalwambo@gmail.com>
2026-05-16 04:21:32 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ let pool;
 
 function getDbConfig() {
  return {
-    host: process.env.DB_HOST || "sdb-84.hosting.stackcp.net",
+    host: process.env.DB_HOST || "mysql.us.stackcp.com:39753",
     port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER || "steve-b80b",
     password: process.env.DB_PASSWORD || "steven2026",
